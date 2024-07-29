f13b85504e85038e320867955e0ca42e71b3677c
minsiksudo <minsik.kim@inha.ac.kr>
2024-07-29 22:37:59 +0900

    trimmomatic path updated to resolve error.

--- COD_sh_20240726_MGK_02_trimmomatic.sh
+++ COD_sh_20240726_MGK_02_trimmomatic.sh
@@ -31,7 +31,7 @@ for file in $RAW_READS_DIR/*_1.fq.gz; do
     echo "Processing ${BASENAME}..."
     
     # Run Trimmomatic with Java memory settings
-    java $JAVA_HEAP_SIZE -jar /path/to/trimmomatic.jar PE -threads $THREADS \
+    java $JAVA_HEAP_SIZE -jar /home/bagel/miniconda3/envs/trimmomatic_env/share/trimmomatic-0.39-2/trimmomatic.jar PE -threads $THREADS \
         ${RAW_READS_DIR}/${BASENAME}_1.fq.gz ${RAW_READS_DIR}/${BASENAME}_2.fq.gz \
         ${FILTERED_DIR}/${BASENAME}_1_paired.fq.gz ${FILTERED_DIR}/${BASENAME}_1_unpaired.fq.gz \
         ${FILTERED_DIR}/${BASENAME}_2_paired.fq.gz ${FILTERED_DIR}/${BASENAME}_2_unpaired.fq.gz \
@@ -47,3 +47,4 @@ done
 
 # Deactivate the Trimmomatic environment
 conda deactivate
+
