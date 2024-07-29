d31ee9c164af5291c2101b634f3185714600b612
minsiksudo <minsik.kim@inha.ac.kr>
2024-07-29 22:36:21 +0900

    Alignment and filtering completed. eggnog database path was updated.

--- COD_sh_20240725_MGK_FastQC_trimmomatic_HISAT2_eggNOG-mapper.sh
+++ COD_sh_20240725_MGK_FastQC_trimmomatic_HISAT2_eggNOG-mapper.sh
@@ -9,28 +9,28 @@
 #fi
 
 # Run Trimmomatic
-echo "Running Trimmomatic..."
-./COD_sh_20240726_MGK_02_trimmomatic.sh
-if [ $? -ne 0 ]; then
-    echo "Error in Trimmomatic step"
-    exit 1
-fi
+#echo "Running Trimmomatic..."
+#./COD_sh_20240726_MGK_02_trimmomatic.sh
+#if [ $? -ne 0 ]; then
+#    echo "Error in Trimmomatic step"
+#    exit 1
+#fi
 
 # Run HISAT2
-echo "Running HISAT2..."
-./COD_sh_20240726_MGK_03_HISAT2.sh
-if [ $? -ne 0 ]; then
-    echo "Error in HISAT2 step"
-    exit 1
-fi
+#echo "Running HISAT2..."
+#./COD_sh_20240726_MGK_03_HISAT2.sh
+#if [ $? -ne 0 ]; then
+#    echo "Error in HISAT2 step"
+#    exit 1
+#fi
 
 # Run Samtools Filtering
-echo "Running Samtools Filtering..."
-./COD_sh_20240726_MGK_04_sam_tools_filtering.sh
-if [ $? -ne 0 ]; then
-    echo "Error in Samtools Filtering step"
-    exit 1
-fi
+#echo "Running Samtools Filtering..."
+#./COD_sh_20240726_MGK_04_sam_tools_filtering.sh
+#if [ $? -ne 0 ]; then
+#    echo "Error in Samtools Filtering step"
+#    exit 1
+#fi
 
 # Run EggNOG-mapper
 echo "Running EggNOG-mapper..."
--- COD_sh_20240726_MGK_05_eggnog_mapper.sh
+++ COD_sh_20240726_MGK_05_eggnog_mapper.sh
@@ -7,6 +7,9 @@ THREADS=40
 FILTERED_DIR="filtered_reads"
 ANNOTATED_DIR="annotated_reads"
 
+# Path to custom EggNOG database directory
+EGGNOG_DB_DIR="/mnt/4T_samsung/Dropbox/Sequencing archive/eggnog_db"
+
 # Create output directory if it doesn't exist
 mkdir -p $ANNOTATED_DIR
 
@@ -14,10 +17,10 @@ mkdir -p $ANNOTATED_DIR
 source /home/bagel/miniconda3/etc/profile.d/conda.sh
 conda activate eggnog_mapper_env
 
-# Run EggNOG-mapper
+# Run EggNOG-mapper with the custom database path
 for file in $FILTERED_DIR/*.bam; do
     BASENAME=$(basename $file _filtered.bam)
-    emapper.py -i $file -o ${ANNOTATED_DIR}/${BASENAME} --cpu $THREADS
+    emapper.py -i $file -o ${ANNOTATED_DIR}/${BASENAME} --cpu $THREADS --data_dir $EGGNOG_DB_DIR
 done
 
 # Deactivate the eggNOG-mapper environment
