5239893de91087db2baf5a7e4bd4d569515a60a1
minsiksudo <minsik.kim@inha.ac.kr>
2024-10-22 15:04:30 +0900

    eggnog mapper updated to version 2.12, and an option to generate gff file was turned on

--- COD_sh_20241022_MGK_eggnog_annotation_to_WGS.sh
+++ COD_sh_20241022_MGK_eggnog_annotation_to_WGS.sh
@@ -3,29 +3,32 @@
 # Script: run_eggnog_annotation.sh
 # Purpose: Automate the process of running EggNOG-mapper and generating functional annotations for Nannochloropsis salina
 
-# Number of threads to use
-THREADS=20
-
 # Directories and input/output files
-REF_DIR="/mnt/4T_samsung/Dropbox/Sequencing_archive/2024_01_02_KNK_NSalina/ref_genome/GCA_004565275.1/ncbi_dataset/data/GCA_004565275.1"
+
+
+# Define variables
+PROTEIN_FILE="/mnt/4T_samsung/Dropbox/Sequencing_archive/2024_01_02_KNK_NSalina/ref_genome/GCA_004565275.1/ncbi_dataset/data/GCA_004565275.1/protein.faa"
+OUTPUT_PREFIX="nannochloropsis_annotation"
 EGGNOG_DB_DIR="/mnt/4T_samsung/Dropbox/Sequencing_archive/eggnog_db"
-PROTEIN_FILE="${REF_DIR}/protein.faa" # Protein sequence file
-OUTPUT_PREFIX="nannochloropsis_annotation" # Output prefix for EggNOG-mapper
+THREADS=40
 
 # EggNOG-mapper command
 echo "Running EggNOG-mapper on ${PROTEIN_FILE} with ${THREADS} threads..."
 
 	source /home/bagel/miniconda3/etc/profile.d/conda.sh
 	conda init
-	conda activate eggnog_mapper_env
+	conda activate eggnog_mapper_py3_env
 
 emapper.py -i ${PROTEIN_FILE} \
            --output ${OUTPUT_PREFIX} \
            --cpu ${THREADS} \
+           -m diamond \
            --data_dir ${EGGNOG_DB_DIR} \
            --dmnd_db ${EGGNOG_DB_DIR}/eggnog_proteins.dmnd \
            --go_evidence experimental \
            --tax_scope auto \
+           --decorate_gff /mnt/4T_samsung/Dropbox/Sequencing_archive/2024_01_02_KNK_NSalina/ref_genome/GCA_004565275.1/ncbi_dataset/data/GCA_004565275.1/genomic_eggnog.gtf \
+	   --override \
            --output_dir ./eggnog_output
 
 if [ $? -eq 0 ]; then
